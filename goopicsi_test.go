d70e2eb569e09784a02e27e48693346c9006ae82
Boris Glimcher <Boris.Glimcher@emc.com>
2022-09-20 22:20:04 +0300

    add test
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

---
+++ goopicsi_test.go
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2022 Dell Inc, or its subsidiaries.
+
+package goopicsi
+
+import (
+	"errors"
+	"strings"
+	"testing"
+)
+
+func TestValidateSomething(t *testing.T) {
+}
