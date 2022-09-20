7453b1d034c27f838b24429fe3f9516895b39d19
Boris Glimcher <Boris.Glimcher@emc.com>
2022-09-20 21:55:18 +0300

    add ConnectToRemoteAndExpose function
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- goopicsi.go
+++ goopicsi.go
@@ -14,3 +14,47 @@ import (
 	pb "github.com/opiproject/opi-api/storage/proto"
 )
 
+func ConnectToRemoteAndExpose(addr string) {
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// Connect to remote NVMe target from xPU
+	c4 := pb.NewNVMfRemoteControllerServiceClient(conn)
+	rr0, err := c4.NVMfRemoteControllerConnect(ctx, &pb.NVMfRemoteControllerConnectRequest{Ctrl: &pb.NVMfRemoteController{Id: 1}})
+	if err != nil {
+		log.Fatalf("could not connect to Remote NVMf controller: %v", err)
+	}
+	log.Printf("Connected: %v", rr0)
+
+	// Expose emulated NVMe device to the Host (Step 1: Subsystem)
+	c1 := pb.NewNVMeSubsystemServiceClient(conn)
+	rs1, err := c1.NVMeSubsystemCreate(ctx, &pb.NVMeSubsystemCreateRequest{Subsystem: &pb.NVMeSubsystem{Nqn: "OpiMalloc7"}})
+	if err != nil {
+		log.Fatalf("could not create NVMe subsystem: %v", err)
+	}
+	log.Printf("Added: %v", rs1)
+	// Step2: NVMeController
+	c2 := pb.NewNVMeControllerServiceClient(conn)
+	rc1, err := c2.NVMeControllerCreate(ctx, &pb.NVMeControllerCreateRequest{Controller: &pb.NVMeController{Name: "OPI-Nvme"}})
+	if err != nil {
+		log.Fatalf("could not create NVMe subsystem: %v", err)
+	}
+	log.Printf("Added: %v", rc1)
+
+	// NVMeNamespace
+	c3 := pb.NewNVMeNamespaceServiceClient(conn)
+	rn1, err := c3.NVMeNamespaceCreate(ctx, &pb.NVMeNamespaceCreateRequest{Namespace: &pb.NVMeNamespace{Name: "OPI-Nvme"}})
+	if err != nil {
+		log.Fatalf("could not create NVMe subsystem: %v", err)
+	}
+	log.Printf("Added: %v", rn1)
+}
