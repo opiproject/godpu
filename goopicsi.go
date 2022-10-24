7ec42e2ae74f0ba4e12d4852cdd4a81ae2180623
Boris Glimcher <Boris.Glimcher@emc.com>
2022-10-24 15:54:58 +0300

    lint: fix some errors
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- goopicsi.go
+++ goopicsi.go
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (c) 2022 Dell Inc, or its subsidiaries.
 
+// Package goopicsi implements the go library for OPI to be used in CSI drivers
 package goopicsi
 
 import (
@@ -14,12 +15,14 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func ConnectToRemoteAndExpose(addr string) {
+// ConnectToRemoteAndExpose connects to the remote storage over NVMe/TCP and exposes it as a local NVMe/PCIe device
+func ConnectToRemoteAndExpose(addr string) error {
 	flag.Parse()
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Printf("did not connect: %v", err)
+		return err
 	}
 	defer func(conn *grpc.ClientConn) {
 		err := conn.Close()
@@ -36,7 +39,8 @@ func ConnectToRemoteAndExpose(addr string) {
 	c4 := pb.NewNVMfRemoteControllerServiceClient(conn)
 	rr0, err := c4.NVMfRemoteControllerConnect(ctx, &pb.NVMfRemoteControllerConnectRequest{Ctrl: &pb.NVMfRemoteController{Id: 1}})
 	if err != nil {
-		log.Fatalf("could not connect to Remote NVMf controller: %v", err)
+		log.Printf("could not connect to Remote NVMf controller: %v", err)
+		return err
 	}
 	log.Printf("Connected: %v", rr0)
 
@@ -44,14 +48,16 @@ func ConnectToRemoteAndExpose(addr string) {
 	c1 := pb.NewNVMeSubsystemServiceClient(conn)
 	rs1, err := c1.NVMeSubsystemCreate(ctx, &pb.NVMeSubsystemCreateRequest{Subsystem: &pb.NVMeSubsystem{Nqn: "OpiMalloc7"}})
 	if err != nil {
-		log.Fatalf("could not create NVMe subsystem: %v", err)
+		log.Printf("could not create NVMe subsystem: %v", err)
+		return err
 	}
 	log.Printf("Added: %v", rs1)
 	// Step2: NVMeController
 	c2 := pb.NewNVMeControllerServiceClient(conn)
 	rc1, err := c2.NVMeControllerCreate(ctx, &pb.NVMeControllerCreateRequest{Controller: &pb.NVMeController{Name: "OPI-Nvme"}})
 	if err != nil {
-		log.Fatalf("could not create NVMe subsystem: %v", err)
+		log.Printf("could not create NVMe subsystem: %v", err)
+		return err
 	}
 	log.Printf("Added: %v", rc1)
 
@@ -59,7 +65,9 @@ func ConnectToRemoteAndExpose(addr string) {
 	c3 := pb.NewNVMeNamespaceServiceClient(conn)
 	rn1, err := c3.NVMeNamespaceCreate(ctx, &pb.NVMeNamespaceCreateRequest{Namespace: &pb.NVMeNamespace{Name: "OPI-Nvme"}})
 	if err != nil {
-		log.Fatalf("could not create NVMe subsystem: %v", err)
+		log.Printf("could not create NVMe subsystem: %v", err)
+		return err
 	}
 	log.Printf("Added: %v", rn1)
+	return nil
 }
