f296abd6af7fbbfbfe3825494e08823bf74cffdb
Boris Glimcher <Boris.Glimcher@emc.com>
2023-02-10 22:16:30 +0200

    Folder restructure
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- csi/doc.go
+++ csi/doc.go
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (c) 2022 Dell Inc, or its subsidiaries.
 
-package ipsec
+// Package csi implements the go library for OPI to be used in CSI drivers
+package csi
