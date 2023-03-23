c3660c195c99f8cbc488873149ed5abb04342e1d
brianroch <rochford.brian@gmail.com>
2023-03-23 12:48:54 +0000

    Adding docker-compose test and making command more consistent
    Signed-off-by: brianroch <rochford.brian@gmail.com>

--- cmd/inventory-get.go
+++ cmd/inventory-get.go
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright (c) 2022-2023 Dell Inc, or its subsidiaries.
+// Copyright (c) 2022 Dell Inc, or its subsidiaries.
 
 // Package cmd implements the CLI commands
 package cmd
 
 import (
-	"fmt"
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"time"
 
 	"github.com/opiproject/godpu/inventory"
 	"github.com/spf13/cobra"
@@ -22,9 +26,27 @@ func NewGetCommand() *cobra.Command {
 		Short:   "Gets DPU inventory information",
 		Args:    cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
-			inventory.SetAddress(addr)
-			res := inventory.Get()
-			fmt.Println(res)
+
+			// Set up a connection to the server.
+			conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if err != nil {
+				log.Fatalf("did not connect: %v", err)
+			}
+			defer func(conn *grpc.ClientConn) {
+				err := conn.Close()
+				if err != nil {
+					log.Fatalf("did not close connection: %v", err)
+				}
+			}(conn)
+
+			// Contact the server and print out its response.
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			err = inventory.Get(ctx, conn)
+			if err != nil {
+				log.Fatalf("could not get inventory: %v", err)
+			}
 		},
 	}
 	flags := cmd.Flags()
--- inventory/inventory.go
+++ inventory/inventory.go
@@ -6,17 +6,9 @@ package inventory
 
 import (
 	"context"
-	"log"
-	"time"
-
 	pb "github.com/opiproject/opi-api/common/v1/gen/go"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-)
-
-var (
-	conn    *grpc.ClientConn
-	address = "localhost:50051"
+	"log"
 )
 
 // SetAddress changes the gRPC connect address of the DPU
@@ -25,41 +17,14 @@ func SetAddress(addr string) {
 }
 
 // Get returns inventory information from DPUs
-func Get() error {
-	if conn == nil {
-		err := dialConnection()
-		if err != nil {
-			return err
-		}
-	}
-
+func Get(ctx context.Context, conn *grpc.ClientConn) error {
 	client := pb.NewInventorySvcClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
 
 	data, err := client.InventoryGet(ctx, &pb.InventoryGetRequest{})
 	if err != nil {
-		log.Println(err)
-	}
-	log.Println(data)
-	defer disconnectConnection()
-	return nil
-}
-
-func dialConnection() error {
-	var err error
-	conn, err = grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Printf("Failed to connect: %v", err)
 		return err
 	}
-	return nil
-}
 
-func disconnectConnection() {
-	err := conn.Close()
-	if err != nil {
-		log.Fatalf("Failed to close connection: %v", err)
-	}
-	log.Println("GRPC connection closed successfully")
+	log.Println(data)
+	return nil
 }
