3f5c17c7b9353458af738c6a67eedea249b4f9ae
Boris Glimcher <36732377+glimchb@users.noreply.github.com>
2023-04-26 12:27:59 -0400

    Create evpn.go
    Signed-off-by: Boris Glimcher <36732377+glimchb@users.noreply.github.com>

---
+++ network/evpn.go
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2022-2023 Dell Inc, or its subsidiaries.
+
+// Package network implements the go library for OPI to be used to establish networking
+package network
