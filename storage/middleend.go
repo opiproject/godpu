a5767c7f6364cbf05b276d220e30883d3795e4a4
Artsiom Koltun <artsiom.koltun@intel.com>
2023-05-11 14:18:11 +0200

    Test QoS volume stats.
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

--- storage/middleend.go
+++ storage/middleend.go
@@ -119,6 +119,11 @@ func executeQosVolume(ctx context.Context, c2 pb.MiddleendQosVolumeServiceClient
 		return err
 	}
 	log.Printf("Got QosVolume: %v", rs5.QosVolumeId.Value)
+	rs6, err := c2.QosVolumeStats(ctx, &pb.QosVolumeStatsRequest{VolumeId: &pc.ObjectKey{Value: "OpiQosVolume3"}})
+	if err != nil {
+		return err
+	}
+	log.Printf("Stats QosVolume: %v", rs6.Stats)
 	rs2, err := c2.DeleteQosVolume(ctx, &pb.DeleteQosVolumeRequest{Name: "OpiQosVolume3"})
 	if err != nil {
 		return err
