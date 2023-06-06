62d68de6b3ea0a68348cf343026b0c91b8be30af
Boris Glimcher <Boris.Glimcher@emc.com>
2023-06-07 00:00:34 +0300

    [middleend] testing with and without {resource}_id field
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- storage/middleend.go
+++ storage/middleend.go
@@ -8,7 +8,9 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"path"
 
+	"github.com/google/uuid"
 	pc "github.com/opiproject/opi-api/common/v1/gen/go"
 	pb "github.com/opiproject/opi-api/storage/v1alpha1/gen/go"
 	"google.golang.org/grpc"
@@ -33,55 +35,67 @@ func executeEncryptedVolume(ctx context.Context, c1 pb.MiddleendEncryptionServic
 	log.Printf("=======================================")
 	log.Printf("Testing NewMiddleendEncryptionServiceClient")
 	log.Printf("=======================================")
-	const name = "opi-encrypted-volume3"
-	rs1, err := c1.CreateEncryptedVolume(ctx, &pb.CreateEncryptedVolumeRequest{
-		EncryptedVolumeId: name,
-		EncryptedVolume: &pb.EncryptedVolume{
-			VolumeId: &pc.ObjectKey{Value: "Malloc1"},
-			Key:      []byte("0123456789abcdef0123456789abcdee"),
-			Cipher:   pb.EncryptionType_ENCRYPTION_TYPE_AES_XTS_128,
-		},
-	})
-	if err != nil {
-		return err
+
+	// testing with and without {resource}_id field
+	for _, resourceID := range []string{"opi-encrypted-volume3", ""} {
+		rs1, err := c1.CreateEncryptedVolume(ctx, &pb.CreateEncryptedVolumeRequest{
+			EncryptedVolumeId: resourceID,
+			EncryptedVolume: &pb.EncryptedVolume{
+				VolumeId: &pc.ObjectKey{Value: "Malloc1"},
+				Key:      []byte("0123456789abcdef0123456789abcdee"),
+				Cipher:   pb.EncryptionType_ENCRYPTION_TYPE_AES_XTS_128,
+			},
+		})
+		if err != nil {
+			return err
+		}
+		// verify
+		newResourceID := resourceID
+		if resourceID == "" {
+			parsed, err := uuid.Parse(path.Base(rs1.Name))
+			if err != nil {
+				return err
+			}
+			newResourceID = parsed.String()
+		}
+		fullname := fmt.Sprintf("//storage.opiproject.org/volumes/%s", newResourceID)
+		if rs1.Name != fullname {
+			return fmt.Errorf("server filled value '%s' is not matching user requested '%s'", rs1.Name, fullname)
+		}
+		log.Printf("Added EncryptedVolume: %v", rs1)
+		rs3, err := c1.UpdateEncryptedVolume(ctx, &pb.UpdateEncryptedVolumeRequest{
+			EncryptedVolume: &pb.EncryptedVolume{
+				Name:     rs1.Name,
+				VolumeId: &pc.ObjectKey{Value: "Malloc1"},
+				Key:      []byte("0123456789abcdef0123456789abcdff"),
+				Cipher:   pb.EncryptionType_ENCRYPTION_TYPE_AES_XTS_128,
+			},
+		})
+		if err != nil {
+			return err
+		}
+		log.Printf("Updated EncryptedVolume: %v", rs3)
+		rs4, err := c1.ListEncryptedVolumes(ctx, &pb.ListEncryptedVolumesRequest{})
+		if err != nil {
+			return err
+		}
+		log.Printf("Listed EncryptedVolume: %v", rs4)
+		rs5, err := c1.GetEncryptedVolume(ctx, &pb.GetEncryptedVolumeRequest{Name: rs1.Name})
+		if err != nil {
+			return err
+		}
+		log.Printf("Got EncryptedVolume: %s", rs5.Name)
+		rs6, err := c1.EncryptedVolumeStats(ctx, &pb.EncryptedVolumeStatsRequest{EncryptedVolumeId: &pc.ObjectKey{Value: rs1.Name}})
+		if err != nil {
+			return err
+		}
+		log.Printf("Stats EncryptedVolume: %s", rs6.Stats)
+		rs2, err := c1.DeleteEncryptedVolume(ctx, &pb.DeleteEncryptedVolumeRequest{Name: rs1.Name})
+		if err != nil {
+			return err
+		}
+		log.Printf("Deleted EncryptedVolume: %v -> %v", rs1, rs2)
 	}
-	fullname := fmt.Sprintf("//storage.opiproject.org/volumes/%s", name)
-	if rs1.Name != fullname {
-		return fmt.Errorf("server filled value '%s' is not matching user requested '%s'", rs1.Name, fullname)
-	}
-	log.Printf("Added EncryptedVolume: %v", rs1)
-	rs3, err := c1.UpdateEncryptedVolume(ctx, &pb.UpdateEncryptedVolumeRequest{
-		EncryptedVolume: &pb.EncryptedVolume{
-			Name:     name,
-			VolumeId: &pc.ObjectKey{Value: "Malloc1"},
-			Key:      []byte("0123456789abcdef0123456789abcdff"),
-			Cipher:   pb.EncryptionType_ENCRYPTION_TYPE_AES_XTS_128,
-		},
-	})
-	if err != nil {
-		return err
-	}
-	log.Printf("Updated EncryptedVolume: %v", rs3)
-	rs4, err := c1.ListEncryptedVolumes(ctx, &pb.ListEncryptedVolumesRequest{})
-	if err != nil {
-		return err
-	}
-	log.Printf("Listed EncryptedVolume: %v", rs4)
-	rs5, err := c1.GetEncryptedVolume(ctx, &pb.GetEncryptedVolumeRequest{Name: fullname})
-	if err != nil {
-		return err
-	}
-	log.Printf("Got EncryptedVolume: %s", rs5.Name)
-	rs6, err := c1.EncryptedVolumeStats(ctx, &pb.EncryptedVolumeStatsRequest{EncryptedVolumeId: &pc.ObjectKey{Value: fullname}})
-	if err != nil {
-		return err
-	}
-	log.Printf("Stats EncryptedVolume: %s", rs6.Stats)
-	rs2, err := c1.DeleteEncryptedVolume(ctx, &pb.DeleteEncryptedVolumeRequest{Name: fullname})
-	if err != nil {
-		return err
-	}
-	log.Printf("Deleted EncryptedVolume: %v -> %v", rs1, rs2)
 	return nil
 }
 
@@ -89,56 +103,68 @@ func executeQosVolume(ctx context.Context, c2 pb.MiddleendQosVolumeServiceClient
 	log.Printf("=======================================")
 	log.Printf("Testing NewMiddleendQosVolumeServiceClient")
 	log.Printf("=======================================")
-	const name = "opi-qos-volume3"
-	rs1, err := c2.CreateQosVolume(ctx, &pb.CreateQosVolumeRequest{
-		QosVolumeId: name,
-		QosVolume: &pb.QosVolume{
-			VolumeId: &pc.ObjectKey{Value: "Malloc1"},
-			MaxLimit: &pb.QosLimit{
-				RwBandwidthMbs: 2,
+
+	// testing with and without {resource}_id field
+	for _, resourceID := range []string{"opi-qos-volume3", ""} {
+		rs1, err := c2.CreateQosVolume(ctx, &pb.CreateQosVolumeRequest{
+			QosVolumeId: resourceID,
+			QosVolume: &pb.QosVolume{
+				VolumeId: &pc.ObjectKey{Value: "Malloc1"},
+				MaxLimit: &pb.QosLimit{
+					RwBandwidthMbs: 2,
+				},
 			},
-		},
-	})
-	if err != nil {
-		return err
-	}
-	fullname := fmt.Sprintf("//storage.opiproject.org/volumes/%s", name)
-	if rs1.Name != fullname {
-		return fmt.Errorf("server filled value '%s' is not matching user requested '%s'", rs1.Name, fullname)
-	}
-	log.Printf("Added QosVolume: %v", rs1)
-	rs3, err := c2.UpdateQosVolume(ctx, &pb.UpdateQosVolumeRequest{
-		QosVolume: &pb.QosVolume{
-			Name:     fullname,
-			VolumeId: &pc.ObjectKey{Value: "Malloc1"},
-			MaxLimit: &pb.QosLimit{
-				RdBandwidthMbs: 2,
+		})
+		if err != nil {
+			return err
+		}
+		// verify
+		newResourceID := resourceID
+		if resourceID == "" {
+			parsed, err := uuid.Parse(path.Base(rs1.Name))
+			if err != nil {
+				return err
+			}
+			newResourceID = parsed.String()
+		}
+		fullname := fmt.Sprintf("//storage.opiproject.org/volumes/%s", newResourceID)
+		if rs1.Name != fullname {
+			return fmt.Errorf("server filled value '%s' is not matching user requested '%s'", rs1.Name, fullname)
+		}
+		log.Printf("Added QosVolume: %v", rs1)
+		rs3, err := c2.UpdateQosVolume(ctx, &pb.UpdateQosVolumeRequest{
+			QosVolume: &pb.QosVolume{
+				Name:     rs1.Name,
+				VolumeId: &pc.ObjectKey{Value: "Malloc1"},
+				MaxLimit: &pb.QosLimit{
+					RdBandwidthMbs: 2,
+				},
 			},
-		},
-	})
-	if err != nil {
-		return err
+		})
+		if err != nil {
+			return err
+		}
+		log.Printf("Updated QosVolume: %v", rs3)
+		rs4, err := c2.ListQosVolumes(ctx, &pb.ListQosVolumesRequest{})
+		if err != nil {
+			return err
+		}
+		log.Printf("Listed QosVolume: %v", rs4)
+		rs5, err := c2.GetQosVolume(ctx, &pb.GetQosVolumeRequest{Name: rs1.Name})
+		if err != nil {
+			return err
+		}
+		log.Printf("Got QosVolume: %v", rs5.Name)
+		rs6, err := c2.QosVolumeStats(ctx, &pb.QosVolumeStatsRequest{VolumeId: &pc.ObjectKey{Value: rs1.Name}})
+		if err != nil {
+			return err
+		}
+		log.Printf("Stats QosVolume: %v", rs6.Stats)
+		rs2, err := c2.DeleteQosVolume(ctx, &pb.DeleteQosVolumeRequest{Name: rs1.Name})
+		if err != nil {
+			return err
+		}
+		log.Printf("Deleted QosVolume: %v -> %v", rs1, rs2)
 	}
-	log.Printf("Updated QosVolume: %v", rs3)
-	rs4, err := c2.ListQosVolumes(ctx, &pb.ListQosVolumesRequest{})
-	if err != nil {
-		return err
-	}
-	log.Printf("Listed QosVolume: %v", rs4)
-	rs5, err := c2.GetQosVolume(ctx, &pb.GetQosVolumeRequest{Name: fullname})
-	if err != nil {
-		return err
-	}
-	log.Printf("Got QosVolume: %v", rs5.Name)
-	rs6, err := c2.QosVolumeStats(ctx, &pb.QosVolumeStatsRequest{VolumeId: &pc.ObjectKey{Value: fullname}})
-	if err != nil {
-		return err
-	}
-	log.Printf("Stats QosVolume: %v", rs6.Stats)
-	rs2, err := c2.DeleteQosVolume(ctx, &pb.DeleteQosVolumeRequest{Name: fullname})
-	if err != nil {
-		return err
-	}
-	log.Printf("Deleted QosVolume: %v -> %v", rs1, rs2)
 	return nil
 }
