a94c5e2e3829cf5952a02967603592726393b6d8
atulpatel261194 <atul.patel@intel.com>
2023-08-10 09:30:33 -0700

    fix(evpn): issue259 consider breaking too long files
    Signed-off-by: atulpatel261194 <atul.patel@intel.com>

---
+++ network/bridge_port.go
@@ -0,0 +1,143 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2022-2023 Intel Corporation, or its subsidiaries.
+// Copyright (c) 2022-2023 Dell Inc, or its subsidiaries.
+
+// Package network implements the go library for OPI to be used to establish networking
+package network
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/opiproject/opi-api/network/evpn-gw/v1alpha1/gen/go"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+)
+
+// CreateBridgePort creates an Bridge Port an OPI server
+func (c evpnClientImpl) CreateBridgePort(ctx context.Context, name string, mac string, bridgePortType string, logicalBridges []string) (*pb.BridgePort, error) {
+	var typeOfPort pb.BridgePortType
+	conn, closer, err := c.NewConn()
+	if err != nil {
+		log.Printf("error creating connection: %s\n", err)
+		return nil, err
+	}
+	defer closer()
+
+	client := c.getEvpnBridgePortClient(conn)
+	switch bridgePortType {
+	case "access":
+		typeOfPort = pb.BridgePortType_ACCESS
+	case "trunk":
+		typeOfPort = pb.BridgePortType_TRUNK
+	default:
+		typeOfPort = pb.BridgePortType_UNKNOWN
+	}
+	data, err := client.CreateBridgePort(ctx, &pb.CreateBridgePortRequest{
+		BridgePortId: name,
+		BridgePort: &pb.BridgePort{
+			Spec: &pb.BridgePortSpec{
+				MacAddress:     []byte(mac),
+				Ptype:          typeOfPort,
+				LogicalBridges: logicalBridges,
+			},
+		},
+	})
+	if err != nil {
+		log.Printf("error creating Bridge Port: %s\n", err)
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// DeleteBridgePort delete an Bridge Port an OPI server
+func (c evpnClientImpl) DeleteBridgePort(ctx context.Context, name string, allowMissing bool) (*emptypb.Empty, error) {
+	conn, closer, err := c.NewConn()
+	if err != nil {
+		log.Printf("error creating connection: %s\n", err)
+		return nil, err
+	}
+	defer closer()
+
+	client := c.getEvpnBridgePortClient(conn)
+	data, err := client.DeleteBridgePort(ctx, &pb.DeleteBridgePortRequest{
+		Name:         resourceIDToFullName("ports", name),
+		AllowMissing: allowMissing,
+	})
+	if err != nil {
+		log.Printf("error deleting  Bridge Port: %s\n", err)
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// GetBridgePort Get Bridge Port details
+func (c evpnClientImpl) GetBridgePort(ctx context.Context, name string) (*pb.BridgePort, error) {
+	conn, closer, err := c.NewConn()
+	if err != nil {
+		log.Printf("error creating connection: %s\n", err)
+		return nil, err
+	}
+	defer closer()
+
+	client := c.getEvpnBridgePortClient(conn)
+	data, err := client.GetBridgePort(ctx, &pb.GetBridgePortRequest{
+		Name: resourceIDToFullName("ports", name),
+	})
+	if err != nil {
+		log.Printf("error getting Bridge Port: %s\n", err)
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// ListBridgePorts list all the Bridge Port an OPI server
+func (c evpnClientImpl) ListBridgePorts(ctx context.Context, pageSize int32, pageToken string) (*pb.ListBridgePortsResponse, error) {
+	conn, closer, err := c.NewConn()
+	if err != nil {
+		log.Printf("error creating connection: %s\n", err)
+		return nil, err
+	}
+	defer closer()
+
+	client := c.getEvpnBridgePortClient(conn)
+	data, err := client.ListBridgePorts(ctx, &pb.ListBridgePortsRequest{
+		PageSize:  pageSize,
+		PageToken: pageToken,
+	})
+	if err != nil {
+		log.Printf("error list Bridge Port: %s\n", err)
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// UpdateBridgePort update the Bridge Port on OPI server
+func (c evpnClientImpl) UpdateBridgePort(ctx context.Context, name string, updateMask []string, allowMissing bool) (*pb.BridgePort, error) {
+	conn, closer, err := c.NewConn()
+	if err != nil {
+		log.Printf("error creating connection: %s\n", err)
+		return nil, err
+	}
+	defer closer()
+
+	client := c.getEvpnBridgePortClient(conn)
+	Port := &pb.BridgePort{
+		Name: resourceIDToFullName("ports", name),
+	}
+	data, err := client.UpdateBridgePort(ctx, &pb.UpdateBridgePortRequest{
+		BridgePort:   Port,
+		UpdateMask:   &fieldmaskpb.FieldMask{Paths: updateMask},
+		AllowMissing: allowMissing,
+	})
+	if err != nil {
+		log.Printf("error updating Bridge Port: %s\n", err)
+		return nil, err
+	}
+
+	return data, nil
+}
---
+++ network/logical_bridge.go
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2022-2023 Intel Corporation, or its subsidiaries.
+// Copyright (c) 2022-2023 Dell Inc, or its subsidiaries.
+
+// Package network implements the go library for OPI to be used to establish networking
+package network
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/opiproject/opi-api/network/evpn-gw/v1alpha1/gen/go"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+)
+
+// CreateLogicalBridge creates an Logical Bridge an OPI server
+func (c evpnClientImpl) CreateLogicalBridge(ctx context.Context, name string, vlanID uint32, vni uint32) (*pb.LogicalBridge, error) {
+	conn, closer, err := c.NewConn()
+	if err != nil {
+		log.Printf("error creating connection: %s\n", err)
+		return nil, err
+	}
+	defer closer()
+
+	client := c.getEvpnLogicalBridgeClient(conn)
+
+	data, err := client.CreateLogicalBridge(ctx, &pb.CreateLogicalBridgeRequest{
+		LogicalBridgeId: name,
+		LogicalBridge: &pb.LogicalBridge{
+			Spec: &pb.LogicalBridgeSpec{
+				VlanId: vlanID,
+				Vni:    vni,
+			},
+		},
+	})
+	if err != nil {
+		log.Printf("error creating logical bridge: %s\n", err)
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// DeleteLogicalBridge deletes an Logical Bridge an OPI server
+func (c evpnClientImpl) DeleteLogicalBridge(ctx context.Context, name string, allowMissing bool) (*emptypb.Empty, error) {
+	conn, closer, err := c.NewConn()
+	if err != nil {
+		log.Printf("error creating connection: %s\n", err)
+		return nil, err
+	}
+	defer closer()
+
+	client := c.getEvpnLogicalBridgeClient(conn)
+
+	data, err := client.DeleteLogicalBridge(ctx, &pb.DeleteLogicalBridgeRequest{
+		Name:         resourceIDToFullName("bridges", name),
+		AllowMissing: allowMissing,
+	})
+	if err != nil {
+		log.Printf("error deleting logical bridge: %s\n", err)
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// GetLogicalBridge get Logical Bridge details from OPI server
+func (c evpnClientImpl) GetLogicalBridge(ctx context.Context, name string) (*pb.LogicalBridge, error) {
+	conn, closer, err := c.NewConn()
+	if err != nil {
+		log.Printf("error creating connection: %s\n", err)
+		return nil, err
+	}
+	defer closer()
+
+	client := c.getEvpnLogicalBridgeClient(conn)
+
+	data, err := client.GetLogicalBridge(ctx, &pb.GetLogicalBridgeRequest{
+		Name: resourceIDToFullName("bridges", name),
+	})
+	if err != nil {
+		log.Printf("error getting logical bridge: %s\n", err)
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// ListLogicalBridges list all Logical Bridge details from OPI server
+func (c evpnClientImpl) ListLogicalBridges(ctx context.Context, pageSize int32, pageToken string) (*pb.ListLogicalBridgesResponse, error) {
+	conn, closer, err := c.NewConn()
+	if err != nil {
+		log.Printf("error creating connection: %s\n", err)
+		return nil, err
+	}
+	defer closer()
+
+	client := c.getEvpnLogicalBridgeClient(conn)
+
+	data, err := client.ListLogicalBridges(ctx, &pb.ListLogicalBridgesRequest{
+		PageSize:  pageSize,
+		PageToken: pageToken,
+	})
+	if err != nil {
+		log.Printf("error List logical bridge: %s\n", err)
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// UpdateLogicalBridge update Logical Bridge on OPI server
+func (c evpnClientImpl) UpdateLogicalBridge(ctx context.Context, name string, updateMask []string) (*pb.LogicalBridge, error) {
+	conn, closer, err := c.NewConn()
+	if err != nil {
+		log.Printf("error creating connection: %s\n", err)
+		return nil, err
+	}
+	defer closer()
+
+	client := c.getEvpnLogicalBridgeClient(conn)
+	Bridge := &pb.LogicalBridge{
+		Name: resourceIDToFullName("bridges", name),
+	}
+	data, err := client.UpdateLogicalBridge(ctx, &pb.UpdateLogicalBridgeRequest{
+		LogicalBridge: Bridge,
+		UpdateMask:    &fieldmaskpb.FieldMask{Paths: updateMask},
+	})
+	if err != nil {
+		log.Printf("error Update logical bridge: %s\n", err)
+		return nil, err
+	}
+
+	return data, nil
+}
