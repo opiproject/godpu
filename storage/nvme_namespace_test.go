b3e0bcee28b81a107405fea1d6acd8c67851993b
Artsiom Koltun <artsiom.koltun@intel.com>
2023-11-23 08:42:50 +0100

    test(storage/nvme): add client create namespace tests
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

---
+++ storage/nvme_namespace_test.go
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 Intel Corporation
+
+// Package storage implements the go library for OPI to be used in storage, for example, CSI drivers
+package storage
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/opiproject/godpu/mocks"
+	pb "github.com/opiproject/opi-api/storage/v1alpha1/gen/go"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
+)
+
+func TestCreateNvmeNamespace(t *testing.T) {
+	namespaceID := "namespace0"
+	volume := "vol0"
+	subsystem := "subsys0Name"
+	testNamespace := &pb.NvmeNamespace{
+		Spec: &pb.NvmeNamespaceSpec{
+			VolumeNameRef: volume,
+		},
+	}
+
+	tests := map[string]struct {
+		giveClientErr    error
+		giveConnectorErr error
+		wantErr          error
+		wantRequest      *pb.CreateNvmeNamespaceRequest
+		wantResponse     *pb.NvmeNamespace
+		wantConnClosed   bool
+	}{
+		"successful call": {
+			giveConnectorErr: nil,
+			giveClientErr:    nil,
+			wantErr:          nil,
+			wantRequest: &pb.CreateNvmeNamespaceRequest{
+				Parent:          subsystem,
+				NvmeNamespaceId: namespaceID,
+				NvmeNamespace:   proto.Clone(testNamespace).(*pb.NvmeNamespace),
+			},
+			wantResponse:   proto.Clone(testNamespace).(*pb.NvmeNamespace),
+			wantConnClosed: true,
+		},
+		"client err": {
+			giveConnectorErr: nil,
+			giveClientErr:    errors.New("Some client error"),
+			wantErr:          errors.New("Some client error"),
+			wantRequest: &pb.CreateNvmeNamespaceRequest{
+				Parent:          subsystem,
+				NvmeNamespaceId: namespaceID,
+				NvmeNamespace:   proto.Clone(testNamespace).(*pb.NvmeNamespace),
+			},
+			wantResponse:   nil,
+			wantConnClosed: true,
+		},
+		"connector err": {
+			giveConnectorErr: errors.New("Some conn error"),
+			giveClientErr:    nil,
+			wantErr:          errors.New("Some conn error"),
+			wantRequest:      nil,
+			wantResponse:     nil,
+			wantConnClosed:   false,
+		},
+	}
+
+	for testName, tt := range tests {
+		t.Run(testName, func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+
+			mockClient := mocks.NewFrontendNvmeServiceClient(t)
+			if tt.wantRequest != nil {
+				toReturn := proto.Clone(tt.wantResponse).(*pb.NvmeNamespace)
+				mockClient.EXPECT().CreateNvmeNamespace(ctx, tt.wantRequest).
+					Return(toReturn, tt.giveClientErr)
+			}
+
+			connClosed := false
+			mockConn := mocks.NewConnector(t)
+			mockConn.EXPECT().NewConn().Return(
+				&grpc.ClientConn{},
+				func() { connClosed = true },
+				tt.giveConnectorErr,
+			)
+
+			c, _ := NewWithArgs(
+				mockConn,
+				func(grpc.ClientConnInterface) pb.FrontendNvmeServiceClient {
+					return mockClient
+				},
+			)
+
+			response, err := c.CreateNvmeNamespace(ctx, namespaceID, subsystem, volume)
+
+			require.Equal(t, tt.wantErr, err)
+			require.True(t, proto.Equal(response, tt.wantResponse))
+			require.Equal(t, tt.wantConnClosed, connClosed)
+		})
+	}
+}
