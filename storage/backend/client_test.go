49ff5e79a460c07468bfa0c2d559da8f029e8cc1
Artsiom Koltun <artsiom.koltun@intel.com>
2024-01-19 13:05:36 +0100

    test(storage): add backend client tests
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

---
+++ storage/backend/client_test.go
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2024 Intel Corporation
+
+// Package backend implements the go library for OPI backend storage
+package backend
+
+import (
+	"testing"
+)
+
+func TestNewClient(t *testing.T) {
+	tests := map[string]struct {
+		address    string
+		wantErr    bool
+		wantClient bool
+	}{
+		"empty address": {
+			address:    "",
+			wantErr:    true,
+			wantClient: false,
+		},
+		"non-empty address": {
+			address:    "localhost:50051",
+			wantErr:    false,
+			wantClient: true,
+		},
+	}
+
+	for testName, tt := range tests {
+		t.Run(testName, func(t *testing.T) {
+			client, err := New(tt.address)
+			if (err != nil) == !tt.wantErr {
+				t.Errorf("expected err: %v, received: %v", tt.wantErr, err)
+			}
+			if (client != nil) == !tt.wantClient {
+				t.Errorf("expected client: %v, received: %v", tt.wantClient, client)
+			}
+		})
+	}
+}
