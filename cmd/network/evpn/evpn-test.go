607756efbbf1b3024f8fe71d6f5582cda775f1d6
Mark Sanders <marksanders194@gmail.com>
2025-01-17 11:42:17 -0600

    fix(godpu): restore missing test file
    Signed-off-by: Mark Sanders <marksanders194@gmail.com>

---
+++ cmd/network/evpn/evpn-test.go
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2022-2023 Intel Corporation, or its subsidiaries.
+// Copyright (c) 2022-2023 Dell Inc, or its subsidiaries.
+
+// Package evpn implements the evpn related CLI commands
+package evpn
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// NewEvpnCommand tests the EVPN functionality command
+func NewEvpnCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "evpn",
+		Aliases: []string{"g"},
+		Short:   "Tests DPU evpn functionality",
+		Args:    cobra.NoArgs,
+		Run: func(cmd *cobra.Command, _ []string) {
+			err := cmd.Help()
+			if err != nil {
+				log.Fatalf("[ERROR] %s", err.Error())
+			}
+		},
+	}
+	// Bridge cli's
+	cmd.AddCommand(CreateLogicalBridge())
+	cmd.AddCommand(DeleteLogicalBridge())
+	cmd.AddCommand(GetLogicalBridge())
+	cmd.AddCommand(ListLogicalBridges())
+	cmd.AddCommand(UpdateLogicalBridge())
+	// Port cli's
+	cmd.AddCommand(CreateBridgePort())
+	cmd.AddCommand(DeleteBridgePort())
+	cmd.AddCommand(GetBridgePort())
+	cmd.AddCommand(ListBridgePorts())
+	cmd.AddCommand(UpdateBridgePort())
+	// VRF cli's
+	cmd.AddCommand(CreateVRF())
+	cmd.AddCommand(DeleteVRF())
+	cmd.AddCommand(GetVRF())
+	cmd.AddCommand(ListVRFs())
+	cmd.AddCommand(UpdateVRF())
+	// SVI cli's
+	cmd.AddCommand(CreateSVI())
+	cmd.AddCommand(DeleteSVI())
+	cmd.AddCommand(GetSVI())
+	cmd.AddCommand(ListSVIs())
+	cmd.AddCommand(UpdateSVI())
+
+	return cmd
+}
